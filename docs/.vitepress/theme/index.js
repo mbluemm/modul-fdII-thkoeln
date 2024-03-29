b55bfc397fbd5c5dc52488a2b208ccd922ecd89b
Zicke&Bock <144685624+SinaNLRBock@users.noreply.github.com>
2024-03-30 00:57:01 +0100

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import 'docs/.vitepress/theme/index.js'
+import 'assets/common_stlye.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
