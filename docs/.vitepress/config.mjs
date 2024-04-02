ffafb2fa28be2b8bbffb5ae6565c714e3805d2e7
SinaNLRBock <sina.bock@th-koeln.de>
2024-04-02 11:03:19 +0200

    deploy site?

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
   // https:/vitepress.dev/reference/default-theme-config
   themeConfig: {
 
-    logo: '/assets/logo_TH_Koeln.png',
+    logo: 'docs/medien/assets/logo_TH_Koeln.png',
 
     // ︶꒷꒦꒷︶꒷꒦꒷︶꒷︶꒷꒦꒷︶ Navigation
     nav: [
